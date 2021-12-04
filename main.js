c60ce079373685834bf52f70cec061beb01f7973
Krishna-Brijesh <90083498+Krishna-Brijesh@users.noreply.github.com>
2021-12-04 11:24:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ answer_holder = "";
 score = 0;
 
 
+function updateCanvas(){
+    background("white");
+    random__number= math.floor((math.random()* quick_draw_data_set.length)+1);
+console.log(quick_draw_data_set[random__number];
+    document.getElementById('sketch_name').innerHTML = 'sketch to be drawn:'+ sketch;}
+
+    function draw(){
+        check_sketch()
+        if(drawn_sketch==sketch)
+        {
+        answer_holder = "set" 
+        score++;
+        document.getElementById('score').innerHTML='score:'+ score;
+        }
+    }
+    function setup(){
+        canvas = createCanvas(280,280);
+        canvas.center();
+        background("white");
+    }
+
+    function check_sketch(){
+        timer_counter++;
+        document.getElementById('time').innerHTML = "timer:"+ timer_counter;
+        console.log(timer_counter)
+        if(timer_counter=="complrted" answer_holder=="set")
+        {
+            timer_check="";
+            answer_holder="";
+            updateCanvas();
+        }
+    }
+
